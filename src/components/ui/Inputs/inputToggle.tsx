f83060a85d33885a05c88d236f3c238fd6d277b5
mataqque <mataqque.100@gmail.com>
2023-12-01 02:30:30 -0500

    refactor: :sparkles: improve  InputToggle
    use formik with the Component Field

--- src/components/ui/Inputs/inputToggle.tsx
+++ src/components/ui/Inputs/inputToggle.tsx
@@ -1,9 +1,11 @@
-import { setInputTextProps } from '../../../common/form';
+import { Field, useField } from 'formik';
 
-export const InputToggle = ({ name, form }: { name: string; form?: any }) => {
+export const InputToggle = (props: { name: string; form?: any }) => {
+	const [field, meta, helpers] = useField(props);
+	console.log(field);
 	return (
 		<label className='relative inline-flex items-center cursor-pointer'>
-			<input type='checkbox' value='' className='sr-only peer' {...setInputTextProps(name, form)} />
+			<Field type='checkbox' className='sr-only peer' name={props.name} />
 			<div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-success"></div>
 		</label>
 	);
